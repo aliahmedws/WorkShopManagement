9fd1e1a1ea15a0a37218d935be4dfb68487a3924
Muhammad Uzair <Muh.uzair21@gmail.com>
2026-01-07 07:44:11 +0500

    Added Stage Tabs

--- angular/src/app/production-manager/production-manager.ts
+++ angular/src/app/production-manager/production-manager.ts
@@ -5,10 +5,11 @@ import { SHARED_IMPORTS } from '../shared/shared-imports.constants';
 import { CarDto, CarService, GetCarListInput } from '../proxy/cars';
 import { ConfirmationHelperService } from '../shared/services/confirmation-helper.service';
 import { Incoming } from "./incoming/incoming";
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 
 @Component({
   selector: 'app-production-manager',
-  imports: [...SHARED_IMPORTS, Incoming],
+  imports: [...SHARED_IMPORTS, NzTabsModule, Incoming],
   templateUrl: './production-manager.html',
   styleUrl: './production-manager.scss',
   providers: [ListService],
@@ -18,20 +19,5 @@ export class ProductionManager  {
 
   public readonly list = inject(ListService);
   filters = {} as GetCarListInput;
-  // private readonly carService = inject(CarService);
-  // private readonly confirmation = inject(ConfirmationHelperService);
-
-  // 
-  // @Input() list: PagedResultDto<CarDto> = { items: [], totalCount: 0 };
-  // @Input() filters = {} as GetCarListInput;
-  // @Output() filtersChange = new EventEmitter<GetCarListInput>();
-
-  // // isModalVisible = false;
-  // selectedId?: string;
-
-  // ngOnInit(): void {
-  //   const carStreamCreator = (query: any) => this.carService.getList({ ...query, ...this.filters });
-  //   this.list.hookToQuery(carStreamCreator).subscribe((res) => (this.cars = res));
-  // }
-
+  selectedIndex = 0;
   }
