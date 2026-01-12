11f1b38faf58111aca44bd21e38a5098026b93e8
Muhammad Uzair <Muh.uzair21@gmail.com>
2026-01-12 19:28:16 +0500

    Updated Default Abp password

--- src/WorkShopManagement.Domain/WorkShopManagementConsts.cs
+++ src/WorkShopManagement.Domain/WorkShopManagementConsts.cs
@@ -6,6 +6,6 @@ public static class WorkShopManagementConsts
 {
     public const string DbTablePrefix = "App";
     public const string? DbSchema = null;
-    public const string AdminEmailDefaultValue = IdentityDataSeedContributor.AdminEmailDefaultValue;
-    public const string AdminPasswordDefaultValue = IdentityDataSeedContributor.AdminPasswordDefaultValue;
+    public const string AdminEmailDefaultValue = "Admin";//IdentityDataSeedContributor.AdminEmailDefaultValue;
+    public const string AdminPasswordDefaultValue = "#wsm-SCD*123!"; // IdentityDataSeedContributor.AdminPasswordDefaultValue;
 }
