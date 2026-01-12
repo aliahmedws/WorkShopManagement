1f5d9bb8fb44eb4c7e2edb49beec9db6fa702790
aliahmedsahi <eswstation@gmail.com>
2026-01-12 20:08:30 +0500

    Updated default user and password in project consts
    Update

--- src/WorkShopManagement.Domain/WorkShopManagementConsts.cs
+++ src/WorkShopManagement.Domain/WorkShopManagementConsts.cs
@@ -6,6 +6,6 @@ public static class WorkShopManagementConsts
 {
     public const string DbTablePrefix = "App";
     public const string? DbSchema = null;
-    public const string AdminEmailDefaultValue = "Admin";//IdentityDataSeedContributor.AdminEmailDefaultValue;
-    public const string AdminPasswordDefaultValue = "#wsm-SCD*123!"; // IdentityDataSeedContributor.AdminPasswordDefaultValue;
+    public const string AdminEmailDefaultValue = "Productionmanager@scddirect.com.au";
+    public const string AdminPasswordDefaultValue = "prod-Manager@2026"; // IdentityDataSeedContributor.AdminPasswordDefaultValue;
 }
