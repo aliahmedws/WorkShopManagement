9663bd69010a406645b9c7709f55ca1715c9f8a2
Ali Ahmed <eswstation@gmail.com>
2026-01-14 00:59:20 +0500

    proxies

--- angular/src/app/proxy/bays/models.ts
+++ angular/src/app/proxy/bays/models.ts
@@ -1,6 +1,11 @@
+import type { PagedAndSortedResultRequestDto } from '@abp/ng.core';
 
 export interface BayDto {
   id?: string;
   name?: string;
   isActive: boolean;
+}
+
+export interface GetBayListInput extends PagedAndSortedResultRequestDto {
+  filter?: string;
 }
